f3fe64ff4a8120f7f7c0e0b78c53f3aa902ad1b1
Chris Fallin <cfallin@mozilla.com>
2020-05-19 15:51:03 -0700

    WIP.

--- report.py
+++ report.py
@@ -3,57 +3,76 @@
 import sys
 import os
 
-# returns [{bench, commithash,
+class Avg(object):
+    def __init__(self):
+        self.samples = []
+    def add(self, value):
+        self.samples.append(value)
+    def mean(self):
+        if len(self.samples) == 0:
+            return 0.0
+        else:
+            return float(sum(self.samples)) / len(self.samples)
+
+class Avgs(object):
+    def __init__(self, columns):
+        self.columns = columns
+        self.avgs = [Avg() for c in columns]
+    def add_line(self, parts):
+        if len(parts) != len(self.avgs):
+            self.avgs = [None for c in self.columns]
+
+        for i in range(len(parts)):
+            if parts[i].strip() == 'FAIL':
+                self.avgs[i] = None
+            else:
+                self.avgs[i].add(float(parts[i]))
+
+    def report(self):
+        avgs = map(lambda a: 'FAIL' if a is None else str(a.mean()), self.avgs)
+        return dict(zip(self.columns, avgs))
+
+    def reset(self):
+        self.avgs = [Avg() for c in self.columns]
+
+
+# returns [{bench,
 #           compile_instructions, compile_cycles, compile_wallclock,
 #           comprun_instructions, comprun_cycles, comprun_wallclock}]
 def get_data(commithash):
     with os.popen("./parse.sh %s" % commithash) as of:
         ret = []
         last_bench = None
-        count = 0
-        comp_cyc_total = 0.0
-        comprun_cyc_total = 0.0
-        comp_ins_total = 0.0
-        comprun_ins_total = 0.0
-        comp_wall_total = 0.0
-        comprun_wall_total = 0.0
-        failed = False
-        for line in of.readlines():
+        avgs = Avgs(['compile_cycles', 'comprun_cycles', 'compile_instructions', 'comprun_instructions', 'compile_wallclock', 'comprun_wallclock'])
+        for line in of.readlines()[1:]:
             parts = line.split(',')
             if len(parts) < 9:
+                print("fail for %s: %s" % (commithash, line))
                 failed = True
                 continue
-
             if parts[0] != commithash:
+                print("fail for %s: %s" % (commithash, line))
                 failed = True
                 continue
 
             bench = parts[1]
             num = parts[2]
 
-            if num == 0:
-                failed = False
-            if any(map(lambda val: val == 'FAIL', parts)):
-                failed = True
-                continue
+            if bench != last_bench:
+                if last_bench != None:
+                    rep = avgs.report()
+                    rep['bench'] = last_bench
+                    ret.append(rep)
+                avgs.reset()
+            last_bench = bench
 
-            comp_cyc_total += float(parts[3])
-            comprun_cyc_total += float(parts[4])
-            comp_ins_total += float(parts[5])
-            comprun_ins_total += float(parts[6])
-            comp_wall_total += float(parts[7])
-            comprun_wall_total += float(parts[8])
-            if not failed and num == 9:
-                ret.append({                                       \
-                    'bench': bench,                                \
-                    'commithash': commithash,                      \
-                    'compile_instructions': comp_ins / 10.0,       \
-                    'comprun_instructions': comprun_ins / 10.0,    \
-                    'compile_cycles': comp_cyc / 10.0,             \
-                    'comprun_cycles': comprun_cyc / 10.0,          \
-                    'compile_wallclock': comp_wallclock / 10.0,    \
-                    'comprun_wallclock': comprun_wallclock / 10.0, \
-                })
+            avgs.add_line(parts[3:])
+
+        if last_bench != None:
+            rep = avgs.report()
+            rep['bench'] = last_bench
+            ret.append(rep)
+
         return ret
 
 # returns [{commithash, desc}]
@@ -62,9 +81,9 @@ def get_commitlist():
         ret = []
         for line in of.readlines():
             parts = line.split(' ', 1)
-            ret.append({                \
-                'commithash': parts[0], \
-                'desc': parts[1],       \
+            ret.append({                        \
+                'commithash': parts[0].strip(), \
+                'desc': parts[1].strip(),       \
             })
         return ret
 
@@ -75,7 +94,7 @@ def report():
     for commit in commits:
         data = get_data(commit['commithash'])
         for row in data:
-            print('%s,"%s",%s,%f,%f,%f,%f,%f,%f' %
+            print('%s,"%s",%s,%s,%s,%s,%s,%s,%s' %
                     (commit['commithash'],
                      commit['desc'],
                      row['bench'],
--- run.sh
+++ run.sh
@@ -31,6 +31,8 @@ echo "Checked out in `pwd`..."
 
 cargo build -p cranelift-tools --release && cargo build --release
 
+export RAYON_NUM_THREADS=1
+
 for bench in $BENCHES; do
   if [ -f $OUT/complete.$bench ]; then
     continue
@@ -40,12 +42,20 @@ for bench in $BENCHES; do
 
   for i in `seq 0 9`; do
     echo "Run $i: $bench: compile time"
-    RAYON_NUM_THREADS=1 perf stat target/release/clif-util wasm --set opt_level=speed --set enable_verifier=false --target aarch64 $ROOT/$bench.wasm 2>$OUT/compile.$bench.$i.txt
+    if ! perf stat \
+         target/release/clif-util wasm --set opt_level=speed --set enable_verifier=false --target aarch64 \
+         $ROOT/$bench.wasm 2>$OUT/compile.$bench.$i.txt; then
+      rm $OUT/compile.$bench.$i.txt
+    fi
   done
 
   for i in `seq 0 9`; do
     echo "Run $i: $bench: compile time + runtime "
-    RAYON_NUM_THREADS=1 perf stat target/release/wasmtime run --disable-cache $ROOT/$bench.wasm 2>$OUT/compile-run.$bench.$i.txt
+    if ! perf stat \
+         target/release/wasmtime run --disable-cache \
+         $ROOT/$bench.wasm 2>$OUT/compile-run.$bench.$i.txt; then
+      rm $OUT/compile-run.$bench.$i.txt
+    fi
   done
 
   touch $OUT/complete.$bench
