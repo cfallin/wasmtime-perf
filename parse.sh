453fc24c56d1bd590be759cbc30e61dc724939a0
Chris Fallin <cfallin@mozilla.com>
2020-05-19 13:25:22 -0700

    Run and parse scripts.

---
+++ parse.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BENCHES="regex-rs bz2"
+FILTER=$1
+if [ "x$FILTER" = "x" ]; then
+  FILTER=""
+fi
+
+get_stat() {
+  file=$1
+  stat=$2
+  if ! grep -q $stat $file; then
+    echo "FAIL"
+    return
+  fi
+  grep $stat $file | awk '{print $1}' | sed -e 's/,//g'
+}
+
+echo "Commit,Bench,Run,Cycles (compile),Cycles (comp+run),Instructions (compile),Instructions (comp+run),Wallclock (compile),Wallclock (comp+run)"
+for dir in data/out.${FILTER}*; do
+  h=`basename $dir`
+  h=`echo $h | sed -e 's/out.//'`
+  for bench in $BENCHES; do
+    for i in `seq 0 9`; do
+      C=$dir/compile.$bench.$i.txt
+      R=$dir/compile-run.$bench.$i.txt
+
+      C_cyc=`get_stat $C cycles`
+      C_inst=`get_stat $C instructions`
+      C_wall=`get_stat $C task-clock`
+      R_cyc=`get_stat $R cycles`
+      R_inst=`get_stat $R instructions`
+      R_wall=`get_stat $R task-clock`
+      echo "$h,$bench,$i,${C_cyc},${R_cyc},${C_inst},${R_inst},${C_wall},${R_wall}"
+    done
+  done
+done
--- run.sh
+++ run.sh
@@ -4,19 +4,22 @@ set -e
 
 # -- config
 REPO=https://github.com/bytecodealliance/wasmtime
-BENCHES="bz2"
+BENCHES="regex-rs bz2"
 # --
 
-if [ $# -lt 2 ]; then
-  echo "Usage: run.sh HASH OUTDIR"
+if [ $# -lt 1 ]; then
+  echo "Usage: run.sh HASH"
 fi
 
 ROOT=`dirname $0`
+ROOT=`readlink -f $ROOT`
 HASH=$1
-OUT=$2
+OUT=$ROOT/data/out.$HASH
 DIR=`mktemp -d`
 
+rm -rf $OUT
 OUT=`readlink -f $OUT`
+mkdir -p $OUT
 
 cd $DIR
 git init wasmtime
@@ -27,16 +30,20 @@ git submodule update --init --depth 1
 
 echo "Checked out in `pwd`..."
 
-cargo build -p cranelift-tools --release
+cargo build -p cranelift-tools --release && cargo build --release
 
 for bench in $BENCHES; do
   for i in `seq 0 9`; do
-    perf stat target/release/clif-util wasm --set opt_level=speed --set enable_verifier=false --target aarch64 $ROOT/$bench.wasm 2>$OUT/compile.$bench.$i.txt
+    echo "Run $i: $bench: compile time"
+    RAYON_NUM_THREADS=1 perf stat target/release/clif-util wasm --set opt_level=speed --set enable_verifier=false --target aarch64 $ROOT/$bench.wasm 2>$OUT/compile.$bench.$i.txt
   done
 
   for i in `seq 0 9`; do
-    perf stat target/release/wasmtime run --disable-cache $ROOT/$bench.wasm 2>$OUT/compile.$bench.$i.txt
+    echo "Run $i: $bench: compile time + runtime "
+    RAYON_NUM_THREADS=1 perf stat target/release/wasmtime run --disable-cache $ROOT/$bench.wasm 2>$OUT/compile-run.$bench.$i.txt
   done
 done
 
 echo "Results in $OUT"
+
+rm -rf $DIR
